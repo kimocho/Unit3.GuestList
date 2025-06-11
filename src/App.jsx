9d7c2afcdb90adea40aa3a314c07aa9f358079a7
Samuel Kim <samjkim455@gmail.com>
2025-06-11 14:18:22 -0400

    got the functionality working at least

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
+import { useState, useEffect } from 'react';
+
 export default function App() {
-  return <></>;
+  const [currentGuest, setCurrentGuest] = useState({});
+  const [wholeData, setCurrentData] = useState([]);
+
+  useEffect(() => {
+    const getting = async () => {
+      const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/guests');
+      const responseJson = await response.json();
+      setCurrentData(responseJson.data);
+    }
+    getting();
+  }, []);
+
+  return (
+    <>
+      <h1>Guest List</h1>
+      {(!currentGuest.name) ?
+        <ul>
+          {
+            wholeData.map((elem) => (
+              <li key={elem.name} onClick={() => setCurrentGuest(elem)}>NAME: {elem.name} EMAIL: {elem.email}</li>
+            ))
+          }
+          <p>Select a guest to see more details.</p>
+        </ul>
+        :
+        <ul>
+          <li key={currentGuest.name}>NAME: {currentGuest.name}</li>
+          <li key={currentGuest.email}>EMAIL: {currentGuest.email}</li>
+          <li key={currentGuest.phone}>PHONE: {currentGuest.phone}</li>
+          <li key={currentGuest.bio}>BIO: {currentGuest.bio}</li>
+          <li key={currentGuest.job}>JOB: {currentGuest.job}</li>
+          <button onClick={() => setCurrentGuest({})}>Back</button>
+        </ul>
+      }
+    </>
+  )
 }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+const listOfGuests = [{ name: 'guest1', email: `a@gmail.com`, phone: '7181234567', bio: 'nothing much', job: 'cashier' },
+{ name: 'guest2', email: 'b@gmail.com', phone: '7182345678', bio: 'i did a few things', job: 'cook' },
+{ name: 'guest3', email: 'c@gmail.com', phone: '3470123456', bio: 'what do you want to know', job: 'teacher' }];
+
+export default listOfGuests;
